2076252e196c121b3b00d0dd6e5e2632171dd12a
tranduythuat <thuattran1810@gmail.com>
2025-09-28 19:57:28 +0700

    minh-huy: update

--- nha-trai/js/index.js
+++ nha-trai/js/index.js
@@ -27,6 +27,76 @@ function initPage() {
     });
 }
 
+// -------------------------
+// 2. Function xử lý khi submit form
+// -------------------------
+async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    console.log("🚀 ~ handleFormSubmit ~ data:", data);
+
+    const {
+        name: name,
+        attendance: confirm,
+        message: message,
+    } = data;
+    console.log("🚀 ~ handleFormSubmit 2~ data:", data);
+
+    // Thông báo khi bắt đầu gửi
+    Swal.fire({
+        title: "Đang gửi /Sending/...",
+        text: "Vui lòng chờ trong giây lát /Please wait a moment/",
+        icon: "info",
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+
+    const url =
+        "https://script.google.com/macros/s/AKfycbxWhX9827DcxfHMs5T_XKcR_pxVHmLhw8l0zMi90YUT7fS0h94kyUGDrt88bSbjOB0g/exec?sheet=nha-trai";
+
+    try {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+                name,
+                confirm,
+                message,
+            }),
+        });
+
+        const result = await res.json().catch(() => ({}));
+        console.log("Server response:", result);
+
+        form.reset();
+
+        // Thông báo thành công
+        Swal.fire({
+            title: "Thành công /Success/!",
+            text: "Cảm ơn bạn đã gửi phản hồi, thông tin đã được gửi đến dâu rể rồi nha /Thank you for your feedback, the information has been sent to the bride and groom./",
+            icon: "success",
+            confirmButtonText: "OK",
+            confirmButtonColor: "#3f4122ff",
+        });
+    } catch (error) {
+        console.error("Error:", error);
+
+        // Thông báo lỗi
+        Swal.fire({
+            title: "Lỗi!",
+            text: "OPPS! Đã xảy ra lỗi: " + error.message,
+            icon: "error",
+            confirmButtonText: "Thử lại",
+            confirmButtonColor: "#3f4122ff",
+        });
+    }
+}
+
 async function toggleMusic(e) {
     const audio = document.getElementById('audio');
     const iconSvg = document.getElementById('iconSvg');
@@ -54,7 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById('player-btn');
     btn.addEventListener('click', toggleMusic);
 
-    const form = document.forms["rsvp-form"];
+    const form = document.forms["rsvpForm"];
     if (form) {
         form.addEventListener("submit", handleFormSubmit);
     }
